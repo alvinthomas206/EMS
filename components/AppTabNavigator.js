945c7254273b52c80b31bd0c2c82f690a087e5e8
ALVIN THOMAS <alvinthomas206@gmail.com>
2021-09-04 22:50:59 +0530

    Add files via upload

---
+++ components/AppTabNavigator.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Image } from 'react-native';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { AppStackNavigator } from './AppStackNavigator'
+import RequestMedicine from '../screens/RequestMedicine';
+
+import UpdateMedicine from '../screens/UpdateMedicines'
+import AddMedicines from '../screens/AddMedicines';
+
+
+export const AppTabNavigator2 = createBottomTabNavigator({
+ 
+  UpdateMedicine : {
+    screen: UpdateMedicine,
+    navigationOptions :{
+      tabBarIcon :   <Image source={require("../assets/home.png")} style={{width:20, height:20}}/>,
+      tabBarLabel : "Update Medicine",
+      
+    }
+  },
+  AddMedicines: {
+    screen: AddMedicines,
+    navigationOptions :{
+      tabBarIcon :<Image source={require("../assets/ads-icon.png")} style={{width:20, height:20,}} />,
+      tabBarLabel : "Add Medicines",
+    }
+  }
+});
+
+
+export const AppTabNavigator = createBottomTabNavigator({
+  HomeScreen : {
+    screen: AppStackNavigator,
+    navigationOptions :{
+      tabBarIcon :   <Image source={require("../assets/home.png")} style={{width:20, height:20}}/>,
+      tabBarLabel : "HomeScreen",
+      
+    }
+  },
+  BookRequest: {
+    screen: RequestMedicine,
+    navigationOptions :{
+      tabBarIcon :<Image source={require("../assets/ads-icon.png")} style={{width:20, height:20,}} />,
+      tabBarLabel : "RequestMedicine",
+    }
+  }
+});
